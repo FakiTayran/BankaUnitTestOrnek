5d2dbb5645a3c774219ab57a50c54e961e7c558a
faki tayran arıduru <fakitayranariduru@hotmail.com>
2021-02-03 12:59:32 +0300

    Add project files.

---
+++ Banka/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Banka
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Hesap hesap1 = new Hesap("Can Halfeoğulları", 200);
+            Console.WriteLine("Hesap Sahibi: " + hesap1.HesapSahibi);
+            Console.WriteLine("Bakiye: {0:n2}₺",hesap1.Bakiye);
+            Console.WriteLine("50 lira para çekelim...");
+            hesap1.ParaCek(50);
+            Console.WriteLine("Bakiye: {0:n2}₺", hesap1.Bakiye);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BankaTestleri/HesapTest.cs
@@ -0,0 +1,42 @@
+﻿using Banka;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BankaTestleri
+{
+    [TestClass]
+    public class HesapTest
+    {
+        [TestMethod]
+        public void ParaCekme_DogruMiktarla_BakiyeGuncelle()
+        {
+            //arrange : test ön hazırlık 
+            decimal baslangicBakiye = 76.40m;
+            decimal cekilecekTutar = 19.63m;
+            decimal beklenen = 56.77m;
+
+            Hesap hesap = new Hesap("Ali Yılmaz", baslangicBakiye);
+
+            //act : eylem
+
+            hesap.ParaCek(cekilecekTutar);
+
+            //assert : iddia
+
+            decimal gercek = hesap.Bakiye;
+            Assert.AreEqual(beklenen, gercek , "Para doğru çekilemedi.");
+        }
+        [TestMethod]
+        public void ParaCek_BakiyeSifirAltinaDuserse_ArgumentOutOfRangeHatasiFirlatmali()
+        {
+            decimal baslangicBakiye = 100;
+            decimal cekilecekTutar = 110;
+
+            Hesap hesap = new Hesap("Ali Yılmaz", baslangicBakiye);
+
+            //act & assert : eylem ve iddia
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => hesap.ParaCek(cekilecekTutar));
+        }
+    }
+}
